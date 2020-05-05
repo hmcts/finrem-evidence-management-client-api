204df3ab63a35d746829b7124a7bfa287e97f5dc
John O'Neill <johnoneill546@gmail.com>
2020-05-05 20:47:04 +0100

    Removed springBoot upgrade

--- src/main/java/uk/gov/hmcts/reform/emclient/configuration/EvidenceManagementClientConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/emclient/configuration/EvidenceManagementClientConfiguration.java
@@ -35,7 +35,7 @@ public class EvidenceManagementClientConfiguration {
     private ObjectMapper objectMapper;
 
     @Autowired
-    private MappingJackson2HttpMessageConverter jackson2HttpCoverter;
+    private MappingJackson2HttpMessageConverter jackson2HttpConverter;
 
     @Value("${http.connect.timeout}")
     private int httpConnectTimeout;
@@ -48,10 +48,10 @@ public class EvidenceManagementClientConfiguration {
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         objectMapper.registerModule(new Jackson2HalModule());
 
-        jackson2HttpCoverter.setObjectMapper(objectMapper);
-        jackson2HttpCoverter.setSupportedMediaTypes(ImmutableList.of(MEDIA_TYPE_HAL_JSON,MediaType.APPLICATION_JSON));
+        jackson2HttpConverter.setObjectMapper(objectMapper);
+        jackson2HttpConverter.setSupportedMediaTypes(ImmutableList.of(MEDIA_TYPE_HAL_JSON,MediaType.APPLICATION_JSON));
 
-        RestTemplate restTemplate = new RestTemplate(asList(jackson2HttpCoverter,
+        RestTemplate restTemplate = new RestTemplate(asList(jackson2HttpConverter,
                 new FormHttpMessageConverter(),
                 new ResourceHttpMessageConverter(),
                 new ByteArrayHttpMessageConverter()));
