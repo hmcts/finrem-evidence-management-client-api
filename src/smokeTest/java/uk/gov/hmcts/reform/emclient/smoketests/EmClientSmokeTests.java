0b56f8c59278ec4fe9979af5c77769d2d94f2f77
Jakub Kozlowski <jakub.kozlowski@gmail.com>
2020-12-04 09:49:35 +0000

    Checkstyle fix

--- src/smokeTest/java/uk/gov/hmcts/reform/emclient/smoketests/EmClientSmokeTests.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/emclient/smoketests/EmClientSmokeTests.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
