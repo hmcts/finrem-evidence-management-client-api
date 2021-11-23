9dd05a3c51f48893133d0e7c90f035b7c3c542c8
Ruban <ruban72@hotmail.com>
2021-11-23 20:00:56 +0000

    DFR-615: Use @Jacksonised for ObjectMapper

--- src/main/java/uk/gov/hmcts/reform/emclient/idam/models/IdamTokens.java
+++ src/main/java/uk/gov/hmcts/reform/emclient/idam/models/IdamTokens.java
@@ -1,14 +1,14 @@
 package uk.gov.hmcts.reform.emclient.idam.models;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
 import java.util.List;
 
 @Data
 @Builder
-@JsonDeserialize(builder = IdamTokens.IdamTokensBuilder.class)
+@Jacksonized
 public class IdamTokens {
     String idamOauth2Token;
     String serviceAuthorization;
--- src/main/java/uk/gov/hmcts/reform/emclient/idam/models/UserDetails.java
+++ src/main/java/uk/gov/hmcts/reform/emclient/idam/models/UserDetails.java
@@ -1,16 +1,16 @@
 package uk.gov.hmcts.reform.emclient.idam.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.extern.jackson.Jacksonized;
 
 import java.util.List;
 
 @Getter
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonDeserialize(builder = UserDetails.UserDetailsBuilder.class)
+@Jacksonized
 public class UserDetails {
 
     private String id;
