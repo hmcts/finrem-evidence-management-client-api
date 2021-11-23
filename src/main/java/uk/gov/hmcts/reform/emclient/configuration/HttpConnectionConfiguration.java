1b1c3f3f62681676b6dd28bf7e56ebfed65b96b7
Ruban <ruban72@hotmail.com>
2021-11-23 17:13:33 +0000

    DFR-615: Fix checkstyle

--- src/main/java/uk/gov/hmcts/reform/emclient/configuration/HttpConnectionConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/emclient/configuration/HttpConnectionConfiguration.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.emclient.config;
+package uk.gov.hmcts.reform.emclient.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
